c98d7abe963af7fb17cbe50764d20ed027a16d2f
mborman <mikewborman@gmailc.om>
2015-06-14 10:43:27 -0500

    added a dependency to see what happens on client and server

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ if (typeof define !== 'function') {
   var define = require('amdefine')(module);
 }
 
-define(function(require) {
+define(['moment'], function(moment) {
 
   return {
     value: function() {
       return "Hello";
+    },
+    timeStamp: function() {
+      return moment().format('MMMM Do YYYY, h:mm:ss a');
     }
   };
 });
